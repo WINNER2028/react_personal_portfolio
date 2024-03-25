4a971c719d99a70e312df0603d51de52814d292e
OBI CHIEMERIE     ("THALES") <obiwinner94@gmail.com>
2024-03-25 11:22:49 +0100

    src Directory created____Components added

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,31 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/About-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+                 Am a self-driven frontend Engineer,
+                 Graphic Designer and web design enthusiast.<br/> 
+                 I love creating amazing Graphic contents
+                 and user friendly web contents... 
+                 
+          </p>
+          <p className="hero--section-description">
+                with spectacular User interface and User Experience.
+                 Have completed several projects, 
+                 both as a Developer and Graphic Designer.
+            <br></br><br></br>  
+                 I collaborate with designers, backend engineers, 
+                 and other stakeholders to ensure that the end result meets both user and business needs. 
+                 Overall, my goal is to make sure that users have an amazing experience when they interact with web applications!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-scroll"
+
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hi, I'm OBI CHIEMERIE</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">FrontEnd</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+             From Imo State, Nigeria.
+            <br /> As a frontend engineer, my job is all about making websites
+             and web applications look great and run smoothly.
+             I work with a variety of technologies like HTML, CSS, JavaScript,
+            <br /> and React to create the user interface of a web application, 
+             making sure that it's not only visually appealing 
+             but also user-friendly and responsive across different devices.
+          </p><br></br>
+        </div>
+        <Link
+              spy={true}
+              smooth={true}
+              to="MyPortfolio"
+              >
+          <button className="btn btn-primary">
+            Check out My Portfolio
+            </button>
+            </Link>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
